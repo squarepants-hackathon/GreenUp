5ba2d769962ad4115ffa61416c15dd3c418ac763
Gagan Vats <gaganvats05@gmail.com>
2022-12-03 22:41:25 +0530

    updated user-model

--- server/model/user-model.js
+++ server/model/user-model.js
@@ -1,13 +1,51 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema(
   {
     email: { type: String, required: true, unique: true },
-    products: [{ type: mongoose.Schema.Types.ObjectId, ref: "prod" }],
+    products: [{ type: mongoose.Schema.Types.ObjectId, ref: 'prod' }],
+    totalWaste: {
+      type: Number,
+      default: 0,
+    },
+    wasteType: {
+      pet: {
+        type: Number,
+        default: 0,
+      },
+      hdpe: {
+        type: Number,
+        default: 0,
+      },
+      pvc: {
+        type: Number,
+        default: 0,
+      },
+      ldpc: {
+        type: Number,
+        default: 0,
+      },
+      pp: {
+        type: Number,
+        default: 0,
+      },
+      ps: {
+        type: Number,
+        default: 0,
+      },
+      other: {
+        type: Number,
+        default: 0,
+      },
+    },
+    RecycledWaste: {
+      type: Number,
+      default: 0,
+    },
   },
   {
     timestamps: true,
   }
 );
 
-module.exports = mongoose.model("users", userSchema);
+module.exports = mongoose.model('users', userSchema);
