f7d0964b949a3e2ad082a45d900ef340ddade9a2
Abhishek Gupta <abhishekgupta942001@gmail.com>
2022-12-03 22:49:16 +0530

    card1 ready

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const Dashboard = () => {
     return (
         <div className="relative w-full lg:p-10 md:p-7 p-5 bg-[#1e1655]">
@@ -11,8 +9,15 @@ const Dashboard = () => {
                 </div>
 
                 <div className="absolute top-[60%] flex flex-row justify-start gap-5 my-4">
+                    <div className="w-[200px] h-[230px] bg-black relative">
+                        <div className="flex justify-end flex-col h-full">
+                            <div className={`hidden_area h-[30%] bg-green-300 w-full`}></div>
+                        </div>
+                        <p className="absolute top-0 left-0 w-full h-full flex justify-center items-center text-xl">45%</p>
+                    </div>
+
                     <div className="w-[200px] h-[230px] bg-black"></div>
-                    <div className="w-[200px] h-[230px] bg-black"></div>
+
                     <div className="w-[200px] h-[230px] flex flex-col justify-between items-center">
                         <div className="w-full h-[47%] bg-black"></div>
                         <div className="w-full h-[47%] bg-black"></div>
