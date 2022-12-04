e530252c50cad58ac17470f4c591c8775596077c
AmitThakur045 <at7129652@gmail.com>
2022-12-04 10:17:38 +0530

    added line chart

---
+++ client/src/Data.js
@@ -0,0 +1,70 @@
+export const UserData = [
+  {
+    id: 1,
+    year: 2016,
+    userGain: 80000,
+    userLost: 823,
+  },
+  {
+    id: 2,
+    year: 2017,
+    userGain: 45677,
+    userLost: 345,
+  },
+  {
+    id: 3,
+    year: 2018,
+    userGain: 78888,
+    userLost: 555,
+  },
+  {
+    id: 4,
+    year: 2019,
+    userGain: 90000,
+    userLost: 4555,
+  },
+  {
+    id: 5,
+    year: 2020,
+    userGain: 4300,
+    userLost: 234,
+  },
+];
+
+export const wasteType = [
+  {
+    id: 1,
+    type: "PET",
+    waste: "100",
+  },
+  {
+    id: 2,
+    type: "HDPE",
+    waste: "140",
+  },
+  {
+    id: 3,
+    type: "PVC",
+    waste: "30",
+  },
+  {
+    id: 4,
+    type: "LDPE",
+    waste: "350",
+  },
+  {
+    id: 4,
+    type: "PP",
+    waste: "400",
+  },
+  {
+    id: 5,
+    type: "PS",
+    waste: "300",
+  },
+  {
+    id: 6,
+    type: "OTHER",
+    waste: "200",
+  },
+];
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,34 @@
-import { CircularProgressbar } from "react-circular-progressbar";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 
 import { BsShare, BsDownload } from "react-icons/bs";
+import LineChart from "../utils/LineChart";
+
+import { wasteType } from "../Data";
+import { useState } from "react";
 
 const Dashboard = () => {
+  const [data, setData] = useState({
+    labels: wasteType.map((data) => data.type),
+    datasets: [
+      {
+        label: "Reducycle Waste",
+        fill: true,
+        tension: 0.2,
+        data: wasteType.map((data) => data.waste),
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "#ecf0f1",
+          "#50AF95",
+          "#f3ba2f",
+          "#2a71d0",
+        ],
+        borderColor: "black",
+        borderWidth: 0.5,
+      },
+    ],
+  });
+
   return (
     <div className="relative w-full lg:p-10 md:p-7 p-5 bg-gradient-to-b from-[#1e1655] to-[#4b4196]">
       <div className="flex flex-col py-8">
@@ -23,9 +48,14 @@ const Dashboard = () => {
 
         <div className="absolute top-[60%] flex flex-row justify-start gap-5 my-4">
           {/* div 1 */}
-          <div className="w-[200px] h-[230px] relative bg-white text-black">
+          <div className="w-[200px] h-[230px] rounded-md relative bg-white text-black">
             <div className="flex justify-end flex-col h-full">
-              <div className={`hidden_area h-[30%] bg-green-300 w-full`}></div>
+              <div
+                style={{
+                  height: "55%",
+                }}
+                className={`hidden_area bg-green-300 rounded-md w-full`}
+              ></div>
             </div>
             <p className="absolute top-0 left-0 w-full h-full flex justify-center items-center text-xl">
               45%
@@ -33,20 +63,37 @@ const Dashboard = () => {
           </div>
 
           {/* div 2 */}
-          <div className="w-[200px] h-[230px] bg-black flex items-center justify-center p-8 bg-gradient-to-t from-cyan-500 to-blue-300">
-            <CircularProgressbar value={"10"} text={`${"10"}%`} />
+          <div className="w-[200px] h-[230px] rounded-md bg-black flex items-center justify-center p-8 bg-gradient-to-t from-cyan-500 to-blue-300">
+            <CircularProgressbar
+              styles={buildStyles({
+                pathColor: `#58a3e8`,
+                textColor: "#ffffff",
+                trailColor: "#ffffff",
+              })}
+              value={"20"}
+              text={`${"20"}%`}
+            />
           </div>
 
           {/* div 3 */}
           <div className="w-[200px] h-[230px] flex flex-col justify-between items-center">
-            <div className="w-full h-[47%] bg-black flex flex-col justify-between p-4 bg-gradient-to-b from-[#362f68] to-gray-400">
+            <div className="w-full h-[47%] bg-black rounded-md flex flex-col justify-between p-4 bg-gradient-to-b from-[#362f68] to-gray-400">
               <p className="text-[10px] uppercase">Live Visitors</p>
               <h1 className="text-2xl text-semibold">10,241</h1>
             </div>
-            <div className="w-full h-[47%] bg-black flex flex-col justify-between p-4 bg-gradient-to-b from-[#362f68] to-gray-400">
+            <div className="w-full h-[47%] bg-black rounded-md flex flex-col justify-between p-4 bg-gradient-to-b from-[#362f68] to-gray-400">
               <p className="text-[10px] uppercase">Bounce</p>
               <h1 className="text-2xl text-semibold">3:42</h1>
             </div>
+          </div>
+        </div>
+
+        <div className="absolute top-[28rem] w-[93vw] h-[20rem] px-5 bg-zinc-200 bg-opacity-40 rounded-xl flex flex-row justify-between items-center">
+          <div className="flex-[0.5] flex flex-row">
+            <LineChart chartData={data} />
+          </div>
+          <div className="flex-[0.5] flex flex-row">
+            <LineChart chartData={data} />
           </div>
         </div>
       </div>
