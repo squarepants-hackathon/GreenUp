8f58f316d53b77f17a8cf7efd75c22a385def819
Abhishek Gupta <abhishekgupta942001@gmail.com>
2022-12-04 13:36:09 +0530

    added authentication using auth0

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,8 +6,8 @@ import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Auth0Provider
-    domain="dev-4utpiwl7wai4et1g.us.auth0.com"
-    clientId="d2elecAI8jrBmxvGMyj8T91ia1eGt4EH"
+    domain={import.meta.env.VITE_AUTH_DOMAIN_ID}
+    clientId={import.meta.env.VITE_AUTH_CLIENT_ID}
     redirectUri={window.location.origin}
   >
     <App />
